7ab4db0c250bf6eea1c21c8dff93c7fd77a481ca
Boongrowth <evelynsoles37@gmail.com>
2026-07-28 01:37:04 +0100

    Delete button-js.js

--- button-js.js
+++
@@ -1,8 +0,0 @@
-function redirectTodeposit() {
-  // Redirect to the desired page
-  window.location.href = "deposit.html";
-}
-function redirectTowithdraw() {
-  // Redirect to the desired page
-  window.location.href = "withdraw.html";
-}
